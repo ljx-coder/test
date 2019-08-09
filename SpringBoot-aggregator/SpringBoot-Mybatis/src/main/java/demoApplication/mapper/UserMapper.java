0b8de79034bf25c775bc618b35f118c8fe935bec
ljx-coder <ljx2273672293@163.com>
2019-08-09 19:17:12 +0800

    修改SQL语句表名

--- SpringBoot-aggregator/SpringBoot-Mybatis/src/main/java/demoApplication/mapper/UserMapper.java
+++ SpringBoot-aggregator/SpringBoot-Mybatis/src/main/java/demoApplication/mapper/UserMapper.java
@@ -14,22 +14,22 @@ import demoApplication.entity.User;
 public interface UserMapper {
 
 	// sql语句写法遵循数据库书写规则
-	@Insert("INSERT INTO user(name,pwd)VALUES(#{name},#{pwd})")
+	@Insert("INSERT INTO userss(name,pwd)VALUES(#{name},#{pwd})")
 	public int add(User user);
 
-	@Delete("DELETE FROM user WHERE uid=#{uid}")
+	@Delete("DELETE FROM userss WHERE uid=#{uid}")
 	public int delete(int id);
 
-	@Update("UPDATE user SET name=#{name},pwd=#{pwd} WHERE uid=#{uid}")
+	@Update("UPDATE userss SET name=#{name},pwd=#{pwd} WHERE uid=#{uid}")
 	public int update(User user);
 
-	@Select("SELECT * FROM user")
+	@Select("SELECT * FROM userss")
 	public List<User> selectAll();
 
-	@Select("SELECT * FROM user WHERE name=#{name}")
+	@Select("SELECT * FROM userss WHERE name=#{name}")
 	public User regist(String name);
 
-	@Select("SELECT * FROM user WHERE name=#{name} AND pwd=#{pwd}")
+	@Select("SELECT * FROM userss WHERE name=#{name} AND pwd=#{pwd}")
 	public User login(String name, String pwd);
 
 }
