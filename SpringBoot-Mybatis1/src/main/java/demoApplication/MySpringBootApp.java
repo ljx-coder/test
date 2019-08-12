427f4f157c73e8253e189f4e119bbaa96ea0b61d
ljx-coder <ljx2273672293@163.com>
2019-08-12 22:25:49 +0800

    SpringBoot-Mybatis

---
+++ SpringBoot-Mybatis1/src/main/java/demoApplication/MySpringBootApp.java
@@ -0,0 +1,14 @@
+package demoApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// @MapperScan
+public class MySpringBootApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBootApp.class, args);
+	}
+
+}
---
+++ SpringBoot-Mybatis1/src/main/java/demoApplication/interceptor/MySpringmvcInterceptor.java
@@ -0,0 +1,42 @@
+package demoApplication.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration // 申明这是一个配置
+public class MySpringmvcInterceptor extends WebMvcConfigurerAdapter {
+
+	// 自定义拦截器
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		HandlerInterceptor handlerInterceptor = new HandlerInterceptor() {
+			@Override
+			public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+					throws Exception {
+				System.out.println("自定义拦截器............");
+				return true;// true表示放行，false表示拦截
+			}
+
+			@Override
+			public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+					ModelAndView modelAndView) throws Exception {
+
+			}
+
+			@Override
+			public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+					Exception ex) throws Exception {
+			}
+		};
+		// 配置拦截器拦截路径
+		registry.addInterceptor(handlerInterceptor).addPathPatterns("/**");
+	}
+
+
+}
