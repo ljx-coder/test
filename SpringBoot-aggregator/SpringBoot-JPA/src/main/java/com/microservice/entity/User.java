546e9c5e40400358f0d5b0bc25a1311ff19017cf
ljx-coder <ljx2273672293@163.com>
2019-08-16 15:30:02 +0800

    原有基础上使用lombok注解简化代码

--- SpringBoot-aggregator/SpringBoot-JPA/src/main/java/com/microservice/entity/User.java
+++ SpringBoot-aggregator/SpringBoot-JPA/src/main/java/com/microservice/entity/User.java
@@ -9,6 +9,9 @@ import javax.persistence.Id;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import lombok.Data;
+
+@Data//lombok注解自动生成getter,setter一系列方法，简化代码
 @Entity
 public class User {
 
@@ -22,14 +25,22 @@ public class User {
 	@Column(nullable = false)
 	private Integer age;
 
-	@Column(nullable = false)
 	@JsonFormat(pattern="yyyy年MM月dd日 HH时",timezone = "GMT+8")
 	private Date birthday;
 
+	@Column(nullable = false)
 	private String address;
 
 	public User() {
 		super();
+	}
+	
+	public User(String name, Integer age, Date birthday, String address) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.birthday = birthday;
+		this.address = address;
 	}
 
 	public User(Long id, String name, Integer age, Date birthday, String address) {
@@ -41,51 +52,7 @@ public class User {
 		this.address = address;
 	}
 
-	public User(String name, Integer age, Date birthday, String address) {
-		super();
-		this.name = name;
-		this.age = age;
-		this.birthday = birthday;
-		this.address = address;
-	}
+	
 
-	public Long getId() {
-		return id;
-	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public Date getBirthday() {
-		return birthday;
-	}
-
-	public void setBirthday(Date birthday) {
-		this.birthday = birthday;
-	}
 }
