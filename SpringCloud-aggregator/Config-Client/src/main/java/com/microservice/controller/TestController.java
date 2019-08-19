57f501f13f73f6e719ace625c8d62d15239bd73e
ljx-coder <ljx2273672293@163.com>
2019-08-19 09:04:34 +0800

    完善项目

---
+++ SpringCloud-aggregator/Config-Client/src/main/java/com/microservice/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.microservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+/*	
+	@Value("${env}")
+	private String env;
+	
+	@RequestMapping("/env")
+	public String hello(){
+		return env;
+	}
+	*/
+
+}
